e6d7c3e3828506b643a6c513770441c07acd2909
Davyd <129197705+SolevarLive@users.noreply.github.com>
2024-03-14 03:37:34 +0500

    fixed errors

--- src/main/java/org/example/controller/RequestController.java
+++ src/main/java/org/example/controller/RequestController.java
@@ -1,15 +1,12 @@
 package org.example.controller;
-
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/requests")
 public class RequestController {
-
     @GetMapping
-    public Map<String,String> getRequest(@RequestHeader Map<String,String> request) {
-        return request;
+    public Map<String, String> getHeaders(@RequestHeader Map<String, String> headers) {
+        return headers;
     }
-
 }
--- src/main/java/org/example/service/OperationServiceDTO.java
+++ src/main/java/org/example/service/OperationServiceDTO.java
@@ -1,7 +1,5 @@
 package org.example.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.dto.Operation;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +8,13 @@ import java.util.List;
 
 @Service
 public class OperationServiceDTO {
-
     List<Operation> operationList = new ArrayList<>();
 
     public void operations(Operation operation) {
-        int id = generateUniqueId();
-        operation.getInfo().setId(id);
-        operationList.add(operation);
+        Operation newOperation = new Operation();
+        newOperation.setInfo(operation.getInfo());
+        newOperation.getInfo().setId(generateUniqueId());
+        operationList.add(newOperation);
     }
 
     private int generateUniqueId() {

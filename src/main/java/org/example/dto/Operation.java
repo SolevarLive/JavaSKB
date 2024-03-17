dc690a415c7b1e304cb7cf3c4bdc2dc6fbada1a4
Davyd <129197705+SolevarLive@users.noreply.github.com>
2024-03-17 21:40:29 +0500

    refactor

--- src/main/java/org/example/dto/Operation.java
+++ src/main/java/org/example/dto/Operation.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.example.common.Info;
+import org.example.validation.MaxAndPositive;
 import org.example.validation.OperationValidation;
 
 @Getter
@@ -13,7 +14,8 @@ import org.example.validation.OperationValidation;
 @OperationValidation
 public class Operation {
 
-    private double price;
+    @MaxAndPositive
+    private int price;
 
     private Info info;
 
--- src/main/java/org/example/validation/MaxAndPositive.java
+++ src/main/java/org/example/validation/MaxAndPositive.java
@@ -2,22 +2,24 @@ package org.example.validation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+@Max(1000)
+@Positive
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE})
-@Constraint(validatedBy = OperationValidator.class)
-public @interface OperationValidation {
-    String message() default "price less than 0";
+@Target({ElementType.FIELD})
+@Constraint(validatedBy = { })
+public @interface MaxAndPositive {
+
+    String message() default "";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
-
 }
